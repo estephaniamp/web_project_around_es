ec6db3651a1c7e35674a9fa09bcca7f9f5ea3bdd
estephaniamp <estephaniamp@gmail.com>
2026-04-22 00:11:13 -0600

    prueba

---
+++ docs/components/UserInfo.js
@@ -0,0 +1,20 @@
+export class UserInfo {
+  constructor({ nameSelector, jobSelector, avatarSelector }) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._jobElement = document.querySelector(jobSelector);
+    this._avatarElement = document.querySelector(avatarSelector);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._nameElement.textContent,
+      job: this._jobElement.textContent,
+    };
+  }
+
+  setUserInfo({ name, job, avatar }) {
+    this._nameElement.textContent = name;
+    this._jobElement.textContent = job;
+    this._avatarElement.src = avatar;
+  }
+}
